cead78b7ca7d7d127858603b7c6706a8b1043b31
James-Kitson <jameskitson10@gmail.com>
2015-12-15 13:49:34 +0000

    figuring out how to aggregate my data

--- collection_analysis.R
+++ collection_analysis.R
@@ -5,3 +5,12 @@ rm(list=ls())
 
 ### Read in my data ####
 collection<-read.csv(file = "Data/2015_collection_data.csv", header = T, stringsAsFactors = F)
+collection$TreeID<-as.character(paste(collection$Pair,collection$Tree, sep="_"))
+
+collection.subs<-subset(collection, c(Visit!="1", Park!="control"))
+
+my.sr2<-aggregate(collection.subs$ID~collection.subs$TreeID+collection.subs$Visit, FUN = function(x) length(unique(x)))
+
+colnames(my.sr2)<-c("TreeID","Visit","SR")
+
+unique(my.sr2$TreeID)
