d266a97ab29c1a6254073d9d8b8210d080979c57
James-Kitson <jameskitson10@gmail.com>
2015-12-15 16:07:55 +0000

    data precessing complete, starting to write models.

--- collection_analysis.R
+++ collection_analysis.R
@@ -22,3 +22,41 @@ collection.met.unique<-unique(collection.met)
 my.sr2<-aggregate(collection.subs$ID~collection.subs$TreeID+collection.subs$Visit, FUN = function(x) length(unique(x)))
 ### rename the columns to something sensible
 colnames(my.sr2)<-c("TreeID","Visit","SR")
+
+my.df<-merge(collection.met.unique,my.sr2, by=c("TreeID","Visit"))
+
+### make a sprayed/unsprayed vector
+for (i in 1:length(my.df$Tree)){
+my.df$Treat[i]<-substring(my.df$Tree[i], first=1, last=nchar(my.df$Tree[i])-1)
+}
+
+### make the metadata into factors
+for (i in c(1:5,7)){
+my.df[,i]<-as.factor(my.df[,i])
+}
+
+### look at how species richness is distributed
+hist(my.df$SR)
+
+### need to check mean and variance for validity of poisson model (i.e. mean should ~= variance)
+mean(my.df$SR)
+var(my.df$SR)
+
+summary(my.df)
+
+model1 <- glm (SR ~ Treat + Park + Visit, 
+               family = poisson (link = sqrt), data = my.df)
+
+AIC(model1)
+
+summary(model1)
+
+par(mfrow=c(2,2))
+plot(model1)
+dev.off()
+
+boxplot(my.df$SR ~ my.df$Visit,
+        ylab="Species Richness", xlab="Visit")
+
+boxplot(my.df$SR ~ my.df$Treat,
+        ylab="Species Richness", xlab="Treatment", notch=T)
