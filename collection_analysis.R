fbc6d14b81033c1dbf2de1d04fd29eed7e998dc7
James-Kitson <jameskitson10@gmail.com>
2015-12-15 15:27:05 +0000

    Finished the script to process the data into species richness

--- collection_analysis.R
+++ collection_analysis.R
@@ -5,12 +5,20 @@ rm(list=ls())
 
 ### Read in my data ####
 collection<-read.csv(file = "Data/2015_collection_data.csv", header = T, stringsAsFactors = F)
+### Make a unique tree ID
 collection$TreeID<-as.character(paste(collection$Pair,collection$Tree, sep="_"))
 
-collection.subs<-subset(collection, c(Visit!="1", Park!="control"))
+### throw away visit one as we aren't using it
+collection.subs<-subset(collection, Visit!="1", select=c("Visit","Park","Pair","Tree","TreeID","ID"))
+### throw away all the control wells as they don't contain organisms
+collection.subs<-subset(collection.subs, Park!="control")
 
+### make a data frame containing only metadata
+collection.met<-subset(collection.subs, select=c("Visit","Park","Pair","Tree","TreeID"))
+### reduce this to unique combinations
+collection.met.unique<-unique(collection.met)
+
+### aggregate the original datafram to count unique values in ID (i.e. specie richness) for each combination of tree and visit
 my.sr2<-aggregate(collection.subs$ID~collection.subs$TreeID+collection.subs$Visit, FUN = function(x) length(unique(x)))
-
+### rename the columns to something sensible
 colnames(my.sr2)<-c("TreeID","Visit","SR")
-
-unique(my.sr2$TreeID)
